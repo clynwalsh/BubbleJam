2ebbeecda48b16d9a1256dd469a0465b73b2cdbe
Carolyn Walsh <cmwalsh126@gmail.com>
2017-07-05 19:37:29 -0400

    Ball moves along path (just the first curve)

--- script.js
+++ script.js
@@ -2,14 +2,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  ctx.beginPath();
-  ctx.moveTo(-20, 200);
-  ctx.bezierCurveTo(200, -20, 775, -30, 840, 200);
-  ctx.bezierCurveTo(960, 700, 100, 625, 80, 400);
-  ctx.bezierCurveTo(40, 190, 300, 100, 550, 150);
-  ctx.bezierCurveTo(900, 200, 700, 650, 250, 375);
-  ctx.lineWidth = 40;
-  ctx.strokeStyle = '#e3ecef';
-  ctx.stroke();
+
+  const spiralPath = () => {
+    ctx.beginPath();
+    ctx.moveTo(-20, 200);
+    ctx.bezierCurveTo(200, -20, 775, -30, 840, 200);
+    ctx.bezierCurveTo(960, 700, 100, 625, 80, 400);
+    ctx.bezierCurveTo(40, 190, 300, 100, 550, 150);
+    ctx.bezierCurveTo(900, 200, 700, 650, 250, 375);
+    ctx.lineWidth = 40;
+    ctx.strokeStyle = '#e3ecef';
+    ctx.stroke();
+  };
+
+  // cubic bezier percent is 0-1
+  // cred to this guy:
+  // https://stackoverflow.com/questions/17083580/i-want-to-do-animation-of-an-object-along-a-particular-path
+  function getCoordsAtPct(start, ctrl1, ctrl2, end, percent){
+    const x = CubicN(percent, start.x, ctrl1.x, ctrl2.x, end.x);
+    const y = CubicN(percent, start.y, ctrl1.y, ctrl2.y, end.y);
+    return {x, y};
+  }
+
+  // cubic helper formula at percent distance
+  function CubicN(percent, a, b, c, d) {
+    const t2 = percent * percent;
+    const t3 = t2 * percent;
+    return a + (-a * 3 + percent * (3 * a - a * percent)) * percent +
+    (3 * b + percent * (-6 * b + b * 3 * percent)) * percent +
+    (c * 3 - c * 3 * percent) * t2 + d * t3;
+  }
+
+  let percent = 0;
+  setInterval( () => {
+
+    const coords = getCoordsAtPct(
+      {x: -20, y: 200},
+      {x: 200, y: -20},
+      {x: 775, y: -30},
+      {x: 840, y: 200},
+      percent
+    );
+
+    const ball = () => {
+      ctx.beginPath();
+      ctx.arc(coords.x, coords.y, 20, 0, Math.PI * 2);
+      ctx.fillStyle = '#4bc1d2';
+      ctx.fill();
+    };
+
+    spiralPath();
+    ball();
+    // good starting speed, should increase over time
+    percent += 0.0005;
+
+  }, 30);
+
 
 });
