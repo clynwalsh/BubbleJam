1fe2dfb780ef280d616db2a02760a59ca499f568
Carolyn Walsh <cmwalsh126@gmail.com>
2017-07-05 20:08:03 -0400

    Ball moves through entire path and stops at the end

--- script.js
+++ script.js
@@ -15,10 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
     ctx.stroke();
   };
 
+  const ball = (coords) => {
+    ctx.beginPath();
+    ctx.arc(coords.x, coords.y, 20, 0, Math.PI * 2);
+    ctx.fillStyle = '#4bc1d2';
+    ctx.fill();
+  };
+
   // cubic bezier percent is 0-1
   // cred to this guy:
   // https://stackoverflow.com/questions/17083580/i-want-to-do-animation-of-an-object-along-a-particular-path
-  function getCoordsAtPct(start, ctrl1, ctrl2, end, percent){
+  function getCoordsAtPct(percent, start, ctrl1, ctrl2, end) {
     const x = CubicN(percent, start.x, ctrl1.x, ctrl2.x, end.x);
     const y = CubicN(percent, start.y, ctrl1.y, ctrl2.y, end.y);
     return {x, y};
@@ -33,30 +40,52 @@ document.addEventListener('DOMContentLoaded', () => {
     (c * 3 - c * 3 * percent) * t2 + d * t3;
   }
 
+  const firstCurve = [
+    {x: -20, y: 200},
+    {x: 200, y: -20},
+    {x: 775, y: -30},
+    {x: 840, y: 200},
+  ];
+
+  const secondCurve = [
+    {x: 840, y: 200},
+    {x: 960, y: 700},
+    {x: 100, y: 625},
+    {x: 80, y: 400}
+  ];
+
+  const thirdCurve = [
+    {x: 80, y: 400},
+    {x: 40, y: 190},
+    {x: 300, y: 100},
+    {x: 550, y: 150}
+  ];
+
+  const fourthCurve = [
+    {x: 550, y: 150},
+    {x: 900, y: 200},
+    {x: 700, y: 650},
+    {x: 250, y: 375}
+  ];
+
+  const curves = [firstCurve, secondCurve, thirdCurve, fourthCurve];
+
   let percent = 0;
-  setInterval( () => {
+  let i = 0;
+  const id = setInterval( () => {
+    if (percent >= 1 && i < 3) {
+      i += 1;
+      percent = 0;
+    } else if (percent >= 1 && i == 3) clearInterval(id);
 
-    const coords = getCoordsAtPct(
-      {x: -20, y: 200},
-      {x: 200, y: -20},
-      {x: 775, y: -30},
-      {x: 840, y: 200},
-      percent
-    );
+    curve = curves[i];
 
-    const ball = () => {
-      ctx.beginPath();
-      ctx.arc(coords.x, coords.y, 20, 0, Math.PI * 2);
-      ctx.fillStyle = '#4bc1d2';
-      ctx.fill();
-    };
+    coords = getCoordsAtPct(percent, ...curve);
 
     spiralPath();
-    ball();
-    // good starting speed, should increase over time
-    percent += 0.0005;
-
+    ball(coords);
+    // .0005 is a good starting speed, should increase over time
+    percent += 0.005;
   }, 30);
-
 
 });
